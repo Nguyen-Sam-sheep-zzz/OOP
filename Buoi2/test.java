5c04ec1a2ce1000080017c20adc2eb241485a429
sheep <samcoxe0307@gmail.com>
2024-04-07 21:55:39 +0700

    baiTapBuoi2

---
+++ Buoi2/test.java
@@ -0,0 +1,16 @@
+import javax.management.MBeanAttributeInfo;
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        int choice = - 1;
+
+        for (int i = 1; i <= 3; ++i) {
+            for (int j = 1; j <= 8; ++j) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        System.out.println("\n----------------------------------");
+    }
+}
